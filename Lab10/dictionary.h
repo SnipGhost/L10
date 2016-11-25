1cd13c50ec54d0de405608fc522c22e0a196838b
SnipGhost <snipghost@list.ru>
2016-11-25 11:29:12 +0400

    Added comments

--- Lab10/dictionary.h
+++ Lab10/dictionary.h
@@ -9,23 +9,24 @@
 //---------------------------------------------------------------------------
 using namespace std;
 //---------------------------------------------------------------------------
-#define DEBUG 1
-#define DICT_LEN 2
-#define STEP_ALLOC 5
+#define DEBUG 1        // Вывод отладочной информации вкл/выкл (1/0)
+#define DICT_LEN 2     // Изначальная длина словаря
+#define STEP_ALLOC 5   // Кол-во довыделяемой памяти за раз
 //--------------------------------------------------------------------------
 struct Word
 {
-	char *en;
-	char *ru;
-	~Word();
+	char *en; // Английское слово
+	char *ru; // Русское слово
+	~Word();  // Следит за принудительным очищением памяти КАЖДОГО слова
 };
 //---------------------------------------------------------------------------
 class Dictionary
 {
 	private:
-		Word **w;             // Массив слов (пары рус-анг)
-		unsigned count, size; // текущее кол-во и размер
-		bool mode;            // 1: EN-RU, 0: RU-EN
+		Word **w;        // Массив слов (пары значений руское:английское)
+		unsigned count;  // Количество уже заданных слов
+		unsigned size;   // Текущее кол-во выделенной памяти
+		bool mode;       // Режим словаря (1: EN-RU, 0: RU-EN)
 
 	public:
 		Dictionary();
@@ -33,11 +34,12 @@ class Dictionary
 		char *viewMode();     // Просмотреть режим словаря
 		void toggleMode();    // Сменить режим словаря
 		void sort(bool type); // Сортировка по алфавиту (0: а-я, 1: я-а)
-		void add(char *eng, char *rus);
-		//unsigned search(char *word);
-		//void del();
-		void print(ostream &out);
-		void loadWords(istream &in);
+		void add(char *eng, char *rus); // Добавить пару анг-рус в словарь
+		//unsigned search(char *word);  // Поиск указанного слова в словаре
+		//void del(char *word);         // Найти и уничтожить все слова word
+		void print(ostream &output); // Распечатать весь словарь
+		void printWord(ostream &output, unsigned index); // Напечатать слово
+		void loadWords(istream &in); // Загрузить слова из потока
 };
 //---------------------------------------------------------------------------
 #endif /* DICTIONARY_H */
